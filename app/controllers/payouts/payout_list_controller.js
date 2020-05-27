67d258ca9567b6f7815d5544584ab945d0944d74
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-05-28 00:34:52 +0100

    PP-6596: payout page includes CSV download link
    Add 'Payout transactions' column to payout page. This filters the
    transaction download by `gateway_payout_id`. To safely perform this
    operation without being loaded into the current gateway account a
    `download/payout/:id` route will need to be added that first fetches the
    payout (to verify it exists and is correct) and then uses the gateway
    account from that entity.

--- app/controllers/payouts/payout_list_controller.js
+++ app/controllers/payouts/payout_list_controller.js
@@ -1,3 +1,4 @@
+const paths = require('../../../app/paths')
 const { response, renderErrorView } = require('../../utils/response.js')
 const { liveUserServicesGatewayAccounts } = require('./../../utils/valid_account_id')
 const payoutService = require('./payouts_service')
@@ -6,7 +7,7 @@ const listAllServicesPayouts = async function listAllServicesPayouts (req, res) 
   try {
     const gatewayAccounts = await liveUserServicesGatewayAccounts(req.user)
     const payoutGroups = await payoutService.payouts(gatewayAccounts.accounts, req.user)
-    response(req, res, 'payouts/list', { payoutGroups })
+    response(req, res, 'payouts/list', { payoutGroups, paths })
   } catch (error) {
     renderErrorView(req, res, 'Failed to fetch payouts')
   }
