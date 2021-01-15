58fec8ee00eee8c945988b9e84ea469d554b0a10
Stephen Daly <stephencdaly@gmail.com>
2021-01-15 14:44:29 +0000

    PP-7583 Move your psp routes
    Update your psp routes to use the new account URL structure.

--- app/routes.js
+++ app/routes.js
@@ -90,7 +90,7 @@ const {
   serviceSwitcher, teamMembers, staticPaths, inviteValidation, editServiceName, merchantDetails,
   notificationCredentials, prototyping, paymentLinks,
   requestToGoLive, policyPages, stripeSetup, stripe,
-  yourPsp, allServiceTransactions, payouts
+  allServiceTransactions, payouts
 } = paths
 const {
   apiKeys,
@@ -100,7 +100,8 @@ const {
   settings,
   toggle3ds,
   toggleBillingAddress,
-  toggleMotoMaskCardNumberAndSecurityCode
+  toggleMotoMaskCardNumberAndSecurityCode,
+  yourPsp
 } = paths.account
 
 // Exports
@@ -180,7 +181,6 @@ module.exports.bind = function (app) {
     ...lodash.values(allServiceTransactions),
     ...lodash.values(credentials),
     ...lodash.values(notificationCredentials),
-    ...lodash.values(apiKeys),
     ...lodash.values(editServiceName),
     ...lodash.values(serviceSwitcher),
     ...lodash.values(teamMembers),
@@ -193,7 +193,6 @@ module.exports.bind = function (app) {
     ...lodash.values(policyPages),
     ...lodash.values(stripeSetup),
     ...lodash.values(stripe),
-    ...lodash.values(yourPsp),
     ...lodash.values(payouts),
     paths.feedback
   ] // Extract all the authenticated paths as a single array
@@ -217,10 +216,10 @@ module.exports.bind = function (app) {
   app.get(payouts.list, permission('transactions:read'), payoutsController.listAllServicesPayouts)
 
   // YOUR PSP
-  app.get(yourPsp.index, permission('gateway-credentials:read'), getAccount, paymentMethodIsCard, yourPspController.getIndex)
-  app.post(yourPsp.worldpay3dsFlex, permission('toggle-3ds:update'), getAccount, paymentMethodIsCard, yourPspController.postToggleWorldpay3dsFlex)
-  app.get(yourPsp.flex, permission('gateway-credentials:update'), getAccount, paymentMethodIsCard, yourPspController.getFlex)
-  app.post(yourPsp.flex, permission('gateway-credentials:update'), getAccount, paymentMethodIsCard, yourPspController.postFlex)
+  account.get(yourPsp.index, permission('gateway-credentials:read'), paymentMethodIsCard, yourPspController.getIndex)
+  account.post(yourPsp.worldpay3dsFlex, permission('toggle-3ds:update'), paymentMethodIsCard, yourPspController.postToggleWorldpay3dsFlex)
+  account.get(yourPsp.flex, permission('gateway-credentials:update'), paymentMethodIsCard, yourPspController.getFlex)
+  account.post(yourPsp.flex, permission('gateway-credentials:update'), paymentMethodIsCard, yourPspController.postFlex)
 
   // CREDENTIALS
   app.get(credentials.index, permission('gateway-credentials:read'), getAccount, paymentMethodIsCard, credentialsController.index)
